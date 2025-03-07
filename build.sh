e7d7528d3b19a67c426cd2997b1c76032621d8ec
Matěj Šťastný <matysta@outlook.com>
2025-03-06 22:14:10 -0800

    Changed folder structure

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build
 
-javac -d build $(find src/java -name "*.java")
+javac -d build $(find src/main/java -name "*.java")
 echo "Main-Class: pokemon.App" >build/MANIFEST.MF
 jar cfm build/pokemon.jar build/MANIFEST.MF -C build .
 
